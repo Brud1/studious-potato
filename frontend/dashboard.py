564304da175c492662aa51913f1fcbab3dcbde30
Brud1 <bradjonjepson@yahoo.co.uk>
2024-07-23 19:49:50 +0100

    optimised mobile

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,13 +1,22 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
 import av
 
 st.title("Home Page")
 
-# Button to access the camera
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
 class VideoProcessor(VideoProcessorBase):
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+webrtc_streamer(
+    key="example",
+    video_processor_factory=VideoProcessor,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={
+        "video": True,
+        "audio": False,
+    }
+)
